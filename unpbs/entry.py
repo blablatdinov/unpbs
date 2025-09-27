4490fbf8f3342a8e9a8e125cf901b96f27664f0b
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-09-27 21:32:32 +0300

    fix test

--- unpbs/entry.py
+++ unpbs/entry.py
@@ -1,6 +1,7 @@
 """Entry point module for coupling analyzer."""
 
 import libcst
+from pathlib import Path
 
 
 def find_function_calls(tree: libcst.Module) -> dict[str, set[str]]:
@@ -68,8 +69,7 @@ def logic(file_content: str) -> str:
 
 
 def main() -> None:
-    """Main entry point for the application.
-
-    TODO #1:30min replace `file.py` with file name from args
-    TODO #1:30min try/except for all cases
-    """
+    """Main entry point for the application."""
+    # TODO #1:30min replace `file.py` with file name from args
+    # TODO #1:30min try/except for all cases
+    print(logic(Path("file.py").read_text()))
